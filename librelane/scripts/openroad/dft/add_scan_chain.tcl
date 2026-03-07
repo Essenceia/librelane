28a6c42652dcbf1f1fa32928afaf29d427c18970
Julia Desmazes - ws <julia.desmazes@gmail.com>
2026-03-07 17:50:01 -0600

    wip

--- librelane/scripts/openroad/dft/add_scan_chain.tcl
+++ librelane/scripts/openroad/dft/add_scan_chain.tcl
@@ -2,6 +2,8 @@ source $::env(SCRIPTS_DIR)/openroad/common/io.tcl
 source $::env(SCRIPTS_DIR)/openroad/dft/utils.tcl 
 source $::env(SCRIPTS_DIR)/openroad/dft/helper.tcl
 
+read_pnr_libs
+read_lefs
 read_current_netlist
 
 set_dft_config -max_chains 1 -clock_mixing no_mix \
